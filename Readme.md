9344c8b3617e1825436fe2abda85bb95315565ba
batman <vlassov@kolesa.kz>
2023-02-15 11:43:22 +0600

    пофиксил крабство

--- Readme.md
+++ Readme.md
@@ -6,13 +6,14 @@
 ipsec status | awk {'print $1'} | grep -v 'Security' | cut -d \"{\" -f1 | cut -d \"[\" -f1 | uniq  | wc -l
 ip xfrm state | grep 'src' |wc -l
 ```
+и возвращает результат в виде метрик, доступных по адресу биндинга, указанного при запуске.
 
-## Ключи.
-Принимается только один аргумент - адрес биндинга для метрик.
+## Аргументы командной строки
+Сервис принимает только один аргумент - адрес биндинга для метрик:
 ```shell
 --bind 0.0.0.0:9090
 ```
-по этому сокету будут доступны метрики.
+По этому сокету будут доступны метрики:
 ```shell
 common-vpn1.alakt.kz.prod.bash.kz:/home/batman# curl common-vpn1.alakt.kz.prod.bash.kz:9090/metrics
 ipsec.vpn.active_tunnels: 2
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use axum::routing::get;
+use axum::Router;
+use
+clap::Parser;
 use std::net::SocketAddr;
 use std::process::Command;
-use axum::Router;
-use axum::routing::get;
-use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
@@ -16,25 +17,30 @@ async fn ipsec_metrics() -> String {
         .arg("-c")
         .arg("ipsec status | awk {'print $1'} | grep -v 'Security' | cut -d \"{\" -f1 | cut -d \"[\" -f1 | uniq  | wc -l")
         .output()
-        .expect("failed to execute command");
+        .expect("Failed to execute command: `ipsec status`");
 
-    let tunnel_count = String::from_utf8(ipsec_status.stdout)
-        .expect("0")
+    let tunnel_count = std::str::from_utf8(&ipsec_status.stdout)
+        .unwrap_or("0")
         .trim()
-        .parse::<i32>()
-        .expect("failed to parse command output as integer");
+        .parse::<u32>()
+        .unwrap_or(0);
 
     let ip_xfrm_state = Command::new("sh")
         .arg("-c")
         .arg("ip xfrm state | grep 'src' |wc -l")
         .output()
-        .expect("failed to execute command");
-    let connections_count = String::from_utf8(ip_xfrm_state.stdout)
-        .expect("0")
+        .expect("Failed to execute command: `ip xfrm state`");
+
+    let connections_count = std::str::from_utf8(&ip_xfrm_state.stdout)
+        .unwrap_or("0")
         .trim()
-        .parse::<i32>()
-        .expect("failed to parse command output as integer");
-    return format!("ipsec.vpn.active_tunnels: {}\nipsec.vpn.active_connection: {}\n", tunnel_count, connections_count)
+        .parse::<u32>()
+        .unwrap_or(0);
+
+    format!(
+        "ipsec.vpn.active_tunnels {}\nipsec.vpn.active_connection: {}\n",
+        tunnel_count, connections_count
+    )
 }
 
 #[tokio::main]
